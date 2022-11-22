f73d7719973f4234ae8aeb3aaf6cc878f0e3cea6
SumiAkter2 <akteralisha36@gmail.com>
2022-11-22 11:20:58 +0600

    added a nav

---
+++ src/Component/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nav-content">
+      <h1>GUN LAGBE</h1>
+      <ul>
+        <li>home</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
