ae1eb0e3fb9e08aaf21770751f0642955db31679
SumiAkter2 <akteralisha36@gmail.com>
2022-11-26 09:15:15 +0600

    added NavLink and class

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { createContext, useState } from "react";
 import Cards from "./Component/Cards";
 import Navbar from "./Component/Navbar";
 import Footer from "./Component/Footer";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./Component/Contact";
+import About from "./Component/About";
 
 export const CartContext = createContext();
 function App() {
@@ -10,13 +13,20 @@ function App() {
   const [cart, setCart] = useState([]);
 
   return (
-    <CartContext.Provider value={[guns, setGuns, cart, setCart]}>
-      <div>
+    <div>
+      <CartContext.Provider value={[guns, setGuns, cart, setCart]}>
         <Navbar />
-        <Cards />
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
         <Footer />
-      </div>
-    </CartContext.Provider>
+      </CartContext.Provider>
+    </div>
   );
 }
 
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,24 +1,46 @@
 import React, { useContext } from "react";
 import { BsCart } from "react-icons/bs";
 import { GiAntiAircraftGun } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 import { CartContext } from "../App";
 
-
-const Navbar = ({openModal}) => {
+const Navbar = ({ openModal }) => {
   const [cart] = useContext(CartContext);
+
   return (
     <div className="nav-content">
       <h1>
         <GiAntiAircraftGun size={40} /> GUN LAGBE
       </h1>
       <ul>
-        <li>Home</li>
-        <li>Contact</li>
-        <li>About</li>
-        <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "Link")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? "active" : "Link")}
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "Link")}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? "active" : "Link")}
+        >
+          LogIn
+        </NavLink>
+        <NavLink className="cart-logo">
           <span>{cart.length}</span>
-          <BsCart size={40} className='cart-logo' onClick={openModal}/>
-        </li>
+          <BsCart size={40} onClick={openModal} />
+        </NavLink>
       </ul>
     </div>
   );
